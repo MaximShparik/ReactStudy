9a181811ee9ed548a6c6046ba8f2ebddf8856004
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 21:01:36 +0300

    Create ProfileStatusHook.jsx

---
+++ src/components/profile/ProfileStatusHook.jsx
@@ -0,0 +1,52 @@
+import React,{useState,useEffect} from 'react';
+import '../.././App.css';
+
+const ProfileStatusHook =(props)=> {
+
+  let [editMode,setEditMode] = useState(false)
+  // урок 84, useState возвращает массив из знвчения и функции
+  // мы присваеваем editMode значение
+  // а setEditMode функцию которая меняет первое значение
+  let [status,setStatus] = useState(props.status)
+
+  useEffect(()=>{
+    setStatus(props.status)
+  },[props.status])
+  // useEffect этот хук запускается когда компонента уже вмонтирована
+  // и он зависит от того что указанно вторым элементом
+  // это нужно чтобы отрисовка не сбоила
+  
+
+  const activateEditMode=()=>{
+    setEditMode(true)
+  }
+
+  const deactivateEditMode=()=>{
+    setEditMode(false)
+    props.UpdateUserStatus(status)
+  }
+
+  const onStatusChange=(e)=>{
+    setStatus(e.currentTarget.value)
+  }
+
+  return (
+      <div className='status'>
+        {!editMode&&
+          <div>
+            <span onDoubleClick={activateEditMode}>{props.status||'---'}</span>
+          </div>
+        }
+        {editMode&&
+          <div>
+            <input onChange={onStatusChange}
+              onBlur={deactivateEditMode}
+              autoFocus={true}
+              value={status}></input>
+          </div>
+        }
+      </div>
+  )
+}
+
+export default ProfileStatusHook;
