a7a5a4e8c33176b044cbf56225078cfcc5575de6
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 21:01:50 +0300

    Create Header.js

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+import '../.././App.css';
+
+const Header = (props) => {
+
+  return (
+    <header className='header'>
+      <img src="logo.png" alt="logo" className='header__logo'></img>
+
+      <div className='header__auth'>
+        {props.isAuth
+          ?<div><p>{props.login}</p><button onClick={props.LogoutHOC}>LogOut</button></div>
+          :<NavLink to='/login'>LogIn</NavLink>}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
