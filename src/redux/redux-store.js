7afcf5ab4100c0955e6eb93958e303f7c3b09a3a
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 21:01:12 +0300

    Create redux-store.js

---
+++ src/redux/redux-store.js
@@ -0,0 +1,25 @@
+import {combineReducers,createStore,applyMiddleware} from 'redux'
+import profileReducer from './profile-reducer'
+import messageReducer from './message-reducer'
+import navbarReducer from './navbar-reducer'
+import usersReducer from './users-reducer'
+import authReducer from './auth-reducer'
+import appReducer from './app-reducer'
+import thunkMiddleware from 'redux-thunk'
+import {reducer as formReducer} from 'redux-form'
+
+let reducers = combineReducers({
+  Profile:profileReducer,
+  Message:messageReducer,
+  Users:usersReducer,
+  Navbar:navbarReducer,
+  Auth:authReducer,
+  App:appReducer,
+  form:formReducer
+});
+
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+window.store = store;
+
+export default store;
