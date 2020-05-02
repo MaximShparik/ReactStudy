e5c19e1ad6e588638828857c39737c5d3f996c69
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 21:01:56 +0300

    Create DialogsName.jsx

---
+++ src/components/dialogs/message/DialogsName.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../.././App.css';
+import {NavLink} from 'react-router-dom'
+
+const DialogsName = (props) => {
+
+  return (
+    <NavLink to={'/dialogs/'+props.id} className='dialogs__item'>{props.name}</NavLink>
+  );
+}
+
+export default DialogsName;
