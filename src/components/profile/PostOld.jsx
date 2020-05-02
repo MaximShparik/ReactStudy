cb758b438ed7303988c7ad49bc9eb0d2ffb5b4fd
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 22:24:22 +0300

    Update PostOld.jsx

--- src/components/profile/PostOld.jsx
+++ src/components/profile/PostOld.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../.././App.css';
+import Ava from '../../img/ava.jpg'
 
 const PostOld = (props) => {
 
   return (
       <div className='post__old'>
-        <img src="ava.jpg" alt="ava" className='post__old-ava'></img>
+        <img src={Ava} alt="ava" className='post__old-ava'></img>
         <p className='post__old-title'>{props.message}</p>
       </div>
   );
