4a44c3a83745abdce9bd7dbc3202c8da9cb96d49
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-05-02 21:41:02 +0300

    Updates

---
+++ precache-manifest.0f0b9dcb5109a8fae70902bff37b8d94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1803e78bd7c1a3afd71ea470f79ec291",
+    "url": "/ReactStudy/index.html"
+  },
+  {
+    "revision": "3059c5c4154db6f8346a",
+    "url": "/ReactStudy/static/css/main.d1cb2274.chunk.css"
+  },
+  {
+    "revision": "af36a6706b3da707e3d5",
+    "url": "/ReactStudy/static/js/2.3a29a93f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactStudy/static/js/2.3a29a93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0b6a2472fcd871a158",
+    "url": "/ReactStudy/static/js/3.bdb2ec9c.chunk.js"
+  },
+  {
+    "revision": "5008d230b27993633699",
+    "url": "/ReactStudy/static/js/4.9a70239f.chunk.js"
+  },
+  {
+    "revision": "3059c5c4154db6f8346a",
+    "url": "/ReactStudy/static/js/main.f944bfff.chunk.js"
+  },
+  {
+    "revision": "c04961eace21060a22c9",
+    "url": "/ReactStudy/static/js/runtime-main.29f4a038.js"
+  },
+  {
+    "revision": "bbb874fe811e4f2b806f45e0987919c7",
+    "url": "/ReactStudy/static/media/ava.bbb874fe.jpg"
+  },
+  {
+    "revision": "4fafd66e4dff9c778691167672d71e9a",
+    "url": "/ReactStudy/static/media/bg.4fafd66e.jpg"
+  },
+  {
+    "revision": "07f42c614c9301c86c84bcb025189310",
+    "url": "/ReactStudy/static/media/preloader.07f42c61.gif"
+  }
+]);
