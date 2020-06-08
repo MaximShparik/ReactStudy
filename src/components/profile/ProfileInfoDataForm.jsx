1b79b6061739c5490deac105389df238cc5fd734
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-06-08 20:15:08 +0200

    Update ProfileInfoDataForm.jsx

--- src/components/profile/ProfileInfoDataForm.jsx
+++ src/components/profile/ProfileInfoDataForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Contacts from './ProfileInfo'
 import {reduxForm,Field} from 'redux-form'
-import {Input} from '../common/formsControler/FormsControler'
+import {Input,Textarea} from '../common/formsControler/FormsControler'
 import {requiredField} from '../../utilits/validators/validators'
 
 const ProfileInfoDataForm =(props)=>{
+
   return(
     <form onSubmit={props.handleSubmit}>
 
@@ -25,24 +26,31 @@ const ProfileInfoDataForm =(props)=>{
       <p>My professional skills:
         <Field placeholder='Your skills'
           name='lookingForAJobDescription'
-          component={Input}
+          component={Textarea}
         />
       </p>
 
       <p>About me:
         <Field placeholder='What do you think about yourself'
           name='aboutMe'
-          component={Input}
+          component={Textarea}
         />
       </p>
+      <p>Contacts: {Object.keys(props.profile.contacts).map(key=>{
+        return <div key={key} className='contact__item'>
+        <p>{key}:<Field component={Input}
+          name={'contacts.'+key}/></p>
+        </div>
+      })}</p>
 
-      /*<p>Contacts: {Object.keys(props.profile.contacts).map(key=>{
-        return <Contacts key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>
-      })}</p>*/
+      {props.error && <div className='form-login-error'>{props.error}</div>}
+
       <button>Save</button>
     </form>
   )
 }
+
+
 
 const ReduxProfileInfoDataForm = reduxForm({
   form:'info'
