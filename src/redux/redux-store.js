7179760b7c597ef1a500a2d0a8f4106767b9036e
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-06-08 20:15:16 +0200

    Update redux-store.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,12 +5,14 @@ import navbarReducer from './navbar-reducer'
 import usersReducer from './users-reducer'
 import authReducer from './auth-reducer'
 import appReducer from './app-reducer'
+import friendsReducer from './friends-reducer'
 import thunkMiddleware from 'redux-thunk'
 import {reducer as formReducer} from 'redux-form'
 
 let reducers = combineReducers({
   Profile:profileReducer,
   Message:messageReducer,
+  Friends:friendsReducer,
   Users:usersReducer,
   Navbar:navbarReducer,
   Auth:authReducer,
