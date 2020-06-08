69335cfc5ef0b5e73e952a1256cdc202c56541b4
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-06-08 20:14:56 +0200

    Update Message.jsx

--- src/components/dialogs/message/Message.jsx
+++ src/components/dialogs/message/Message.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../.././App.css';
+import Ava from '../../../img/ava.jpg'
 
 const Message = (props) => {
 
   return (
     <div className={'message '+ props.class}>
       <div className='message__person'>
-        <img src={props.avasrc} alt="ava" className='message__ava'/>
+        <img src={Ava} alt="ava" className='message__ava'/>
         <span className='message__name'>{props.from}</span>
       </div>
       <div className='message__area'>
