7e5431310b90007dd2432015e685cb0f92a8e315
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-06-12 19:29:44 +0200

    Update PostOld.jsx

--- src/components/profile/PostOld.jsx
+++ src/components/profile/PostOld.jsx
@@ -3,10 +3,16 @@ import '../.././App.css';
 import Ava from '../../img/ava.jpg'
 
 const PostOld = (props) => {
+  // debugger
+  if(props.photo==null||props.photo.photos.small==null){
+    var photo = Ava
+  } else{
+    var photo = props.photo.photos.small
+  }
 
   return (
       <div className='post__old'>
-        <img src={Ava} alt="ava" className='post__old-ava'></img>
+        <img src={photo} alt="ava" className='post__old-ava'></img>
         <p className='post__old-title'>{props.message}</p>
       </div>
   );
