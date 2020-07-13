459e52adb30d47fb1241414b06469736a6140177
MaximShparik <64692970+MaximShparik@users.noreply.github.com>
2020-07-13 15:43:58 +0200

    Update DialogsName.jsx

--- src/components/dialogs/message/DialogsName.jsx
+++ src/components/dialogs/message/DialogsName.jsx
@@ -3,7 +3,7 @@ import '../../.././App.css';
 import {NavLink} from 'react-router-dom'
 
 const DialogsName = (props) => {
-
+  // debugger
   return (
     <NavLink to={'/dialogs/'+props.id} className='dialogs__item'>{props.name}</NavLink>
   );
